37e4d69407510da687a4e3286c812279768965e7
Refat E Ferdous <149683642+ref-wtag@users.noreply.github.com>
2023-11-27 14:11:11 +0600

    initial commit

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -10,49 +10,48 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
    
     @IBOutlet var table : UITableView!
-    var models = [InstragramPost]()
+    var models = [
+     "first",
+    "second",
+    "third",
+    "fourth"
+    ]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        table.register(PostTableViewCell.nib(), forCellReuseIdentifier: PostTableViewCell.identifier)
         table.delegate = self
         table.dataSource = self
-        
-        models.append(InstragramPost(numberOfLikes: 200, userName: "Refat", userImageName: "img_1", postImageName: "img_1"))
-        
-        models.append(InstragramPost(numberOfLikes: 205, userName: "Susmoy", userImageName: "img_2", postImageName: "img_2"))
-        models.append(InstragramPost(numberOfLikes: 300, userName: "Farhana", userImageName: "img_4", postImageName: "img_4"))
-        models.append(InstragramPost(numberOfLikes: 410, userName: "mitu", userImageName: "img_5", postImageName: "img_5"))
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        models.count
+        return models.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier, for: indexPath) as! PostTableViewCell
-        cell.configure(with: models[indexPath.row])
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for : indexPath)
+        cell.textLabel?.text = models[indexPath.row]
+        cell.textLabel?.font = UIFont(name: "Arial", size: 22)
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 120+140+view.frame.size.width
+
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        models.swapAt(sourceIndexPath.row, destinationIndexPath.row)
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+    
+    @IBAction func didTapSort(){
+        if table.isEditing{
+            table.isEditing = false
+        }
+        else{
+            table.isEditing = true
+        }
     }
     
 }
 
-struct InstragramPost{
-    let numberOfLikes : Int
-    let userName : String
-    let userImageName : String
-    let postImageName : String
-}
 
